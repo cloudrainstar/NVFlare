aa4ff2155e2b8e191bea2311100d46384408e06d
Ken Liao <cloudrainstar@gmail.com>
2022-06-13 10:52:32 +0800

    Add hello-monai-tb example

---
+++ examples/hello-monai-tb/custom/extra_executor.py
@@ -0,0 +1,43 @@
+import os
+
+from monai.apps.utils import download_and_extract
+
+from nvflare.apis.executor import Executor
+from nvflare.apis.fl_constant import FLContextKey, ReturnCode
+from nvflare.apis.fl_context import FLContext
+from nvflare.apis.shareable import Shareable
+from nvflare.apis.signal import Signal
+
+
+class ExtraExecutor(Executor):
+    def execute(
+        self,
+        task_name: str,
+        shareable: Shareable,
+        fl_ctx: FLContext,
+        abort_signal: Signal,
+    ) -> Shareable:
+        if task_name == "EXTRA_PREPROCESS_TASK":
+            self.log_info(fl_ctx, "THIS IS AN EXTRA WORKFLOW TASK RECEIVED BY THE CLIENT.")
+            # Extract info from Sharable
+            url = shareable.get_header("url")
+            dataset_folder_name = shareable.get_header("dataset_folder_name")
+            # Extract info from FLContext
+            workspace = fl_ctx.get_prop(FLContextKey.WORKSPACE_OBJECT)
+            dataset_root = workspace.get_root_dir()
+            # Combine dataset
+            dataset_path = os.path.join(dataset_root, dataset_folder_name)
+            if not os.path.exists(dataset_path):
+                self.download_spleen_dataset(url, dataset_root, dataset_path)
+            shareable = Shareable()
+            shareable.set_return_code(ReturnCode.OK)
+            return shareable
+        else:
+            # If unknown task name, set ReturnCode accordingly.
+            shareable = Shareable()
+            shareable.set_return_code(ReturnCode.TASK_UNKNOWN)
+            return shareable
+
+    def download_spleen_dataset(self, url: str, dataset_root: str, dataset_path: str):
+        tarfile_name = f"{dataset_path}.tar"
+        download_and_extract(url=url, filepath=tarfile_name, output_dir=dataset_root)
---
+++ examples/hello-monai-tb/custom/extra_workflow.py
@@ -0,0 +1,55 @@
+from nvflare.apis.client import Client
+from nvflare.apis.fl_context import FLContext
+from nvflare.apis.impl.controller import Controller, Task
+from nvflare.apis.shareable import Shareable
+from nvflare.apis.signal import Signal
+
+
+class ExtraWorkflow(Controller):
+    def __init__(self):
+        Controller.__init__(self)
+
+    def start_controller(self, fl_ctx: FLContext) -> None:
+        self.log_info(fl_ctx, "Starting extra workflow.")
+
+    def control_flow(self, abort_signal: Signal, fl_ctx: FLContext) -> None:
+        # Start an extra task from the server
+        self.log_info(fl_ctx, "THIS IS AN EXTRA WORKFLOW TASK GIVEN BY THE SERVER.")
+
+        # Create a Shareable and add to a task
+        # Set the url and dataset path for the workers
+        url = "https://msd-for-monai.s3-us-west-2.amazonaws.com/Task09_Spleen.tar"
+        dataset_folder_name: str = "Task09_Spleen"
+        # We can enter that into a shareable
+        shareable = Shareable()
+        shareable.set_header("url", url)
+        shareable.set_header("dataset_folder_name", dataset_folder_name)
+        extra_task = Task(
+            name="EXTRA_PREPROCESS_TASK",
+            data=shareable,
+            props={},
+            timeout=300,
+        )
+
+        # Send the task and wait for results
+        self.broadcast_and_wait(
+            task=extra_task,
+            min_responses=1,
+            wait_time_after_min_received=30,
+            fl_ctx=fl_ctx,
+            abort_signal=abort_signal,
+        )
+
+        # Finally, exit
+        return
+
+    def stop_controller(self, fl_ctx: FLContext):
+        self.cancel_all_tasks(fl_ctx=fl_ctx)
+
+    def handle_event(self, event_type: str, fl_ctx: FLContext):
+        super().handle_event(event_type, fl_ctx)
+
+    def process_result_of_unknown_task(
+        self, client: Client, task_name: str, client_task_id: str, result: Shareable, fl_ctx: FLContext
+    ):
+        self.log_info(fl_ctx, "Ignoring result from unknown task.")
